ce583358f0730e604f772c41c086fa56faa4aa01
dusantavic <dusantavic1@gmail.com>
2023-12-15 14:53:21 +0100

    clojureselect data

--- test/clojureselect/core_test.clj
+++ test/clojureselect/core_test.clj
@@ -1,6 +1,6 @@
 (ns clojureselect.core-test
   (:require [clojure.test :refer :all]
-            [clojureselect.core :refer :all]))
+            [clojureselect.data :refer :all]))
 
 (deftest a-test
   (testing "FIXME, I fail."
