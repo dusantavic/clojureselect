693395a9bef604d4c80061dd1b70fa67af52f51b
dusantavic <dusantavic1@gmail.com>
2023-12-16 12:59:50 +0100

    repository

--- project.clj
+++ project.clj
@@ -3,7 +3,10 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.11.1"]]
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [com.github.seancorfield/next.jdbc "1.3.894"],
+                 [org.clojure/java.jdbc "0.7.12"]
+                 [mysql/mysql-connector-java "8.0.33"]]
   :main ^:skip-aot clojureselect.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
--- src/clojureselect/repository.clj
+++ src/clojureselect/repository.clj
@@ -1,2 +1,39 @@
-(ns clojureselect.repository)
+(ns clojureselect.repository 
+  (:require [next.jdbc :as jdbc-next]
+            [next.jdbc.result-set :as rs]))
+
+(def db-spec
+  {:dbtype "mysql"
+   :dbname "clojureselect"
+   :user "root"
+   :password "dusan"
+   :host "localhost"
+   :port 3306})
+
+(def db (jdbc-next/get-datasource db-spec))
+
+(defn get-candidate
+  [candidate-id]
+  (jdbc-next/execute-one! db
+                          ["SELECT * FROM candidates WHERE id = ?" candidate-id] {:builder-fn rs/as-unqualified-maps}))
+
+(defn get-candidates []
+  (jdbc-next/execute! db
+                                ["SELECT * FROM candidates"] {:builder-fn rs/as-unqualified-maps}))
+
+(defn get-jobs []
+  (jdbc-next/execute! db
+                          ["SELECT * FROM jobs"] {:builder-fn rs/as-unqualified-maps}))
+
+(defn get-ratings []
+  (jdbc-next/execute! db
+                          ["SELECT * FROM ratings"] {:builder-fn rs/as-unqualified-maps}))
+
+(defn get-criteria []
+  (jdbc-next/execute! db
+                          ["SELECT * FROM criteria"] {:builder-fn rs/as-unqualified-maps}))
+
+(defn get-qualifications []
+  (jdbc-next/execute! db
+                          ["SELECT * FROM qualifications"] {:builder-fn rs/as-unqualified-maps}))
 
